ab66c464162d956c90b402a3bd0ab904659506f1
Jesse Chase <jesse@digitalocean.com>
2016-01-24 18:24:30 -0500

    smaller margin on mobile

--- config.rb
+++ config.rb
@@ -77,7 +77,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  activate :relative_assets
+  activate :relative_assets, true
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
